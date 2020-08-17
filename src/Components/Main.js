a8124d21e06fab7891ef4bd1b5b4bcd22ec5c19b
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2020-08-17 20:09:41 +0600

    initialized initial states

---
+++ src/Components/Main.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+function Main(){
+	
+	const [countries, setCountries] 	  = useState([]);
+	const [defaultCntry, setDefaultCntry] = useState("Kyrgyzstan"); 
+
+	let getAllCountries = async() => {
+		
+		try {
+
+		} catch(err) {
+			console.log(err);
+		}
+	}
+
+	return 	<div>
+
+	   		</div>
+}
+
+export default Main;
